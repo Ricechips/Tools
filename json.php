556c8d4156d92a26e9e53d02d6042878e41ab841
yanzhifanghuoqiang <shiyuhong@hdu.edu.cn>
2019-11-18 15:23:00 +0800

    Update json.php

--- json.php
+++ json.php
@@ -1,20 +1,20 @@
 <?php
 
-//JSON format
-//[1,2,5,7,8,"Hello",[6,7,8],{"h":"Hello"}]
-//{"h":"Hello","w":"World",[1,2,3]}
+// JSON format
+// [1,2,5,7,8,"Hello",[6,7,8],{"h":"Hello"}]
+// {"h":"Hello","w":"World",[1,2,3]}
 
-//encode >>>>>>>>>>>>>>>>>>>>>
+// encode >>>>>>>>>>>>>>>>>>>>>
 
-//$arr = array(1,2,5,8,'Hello','Ricechips',array('h'=>'Hello','name'=>'Ricechips'));
-//print_r($arr);
-//echo json_encode($arr);
+// $arr = array(1,2,5,8,'Hello','Ricechips',array('h'=>'Hello','name'=>'Ricechips'));
+// print_r($arr);
+// echo json_encode($arr);
 
-//$obj = array('h'=>'Hello','w'=>'World',array(3,2,1));
-//echo json_encode($obj);
+// $obj = array('h'=>'Hello','w'=>'World',array(3,2,1));
+// echo json_encode($obj);
 
-//decode
+// decode
 $jsonStr = '{"h":"Hello","w":"World","0":[3,2,1]}';
 $obj = json_decode($jsonStr);
-//print_r($obj);
+// print_r($obj);
 echo $obj->h;
