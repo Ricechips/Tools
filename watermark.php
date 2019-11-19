428c2f2b4aa1d42b452d2bc31091badd5c1e6db4
yanzhifanghuoqiang <shiyuhong@hdu.edu.cn>
2019-11-19 17:15:47 +0800

    Update watermark.php

--- watermark.php
+++ watermark.php
@@ -6,3 +6,6 @@ imagestring($img,5,5,5,'Ricechips',imagecolorallocate($img,0,255,0));
 
 header('Content-type: image/png');
 imagepng($img);
+
+
+// ps: black 100,116,163 ; red 255,0,0 ; white 255,255,255
